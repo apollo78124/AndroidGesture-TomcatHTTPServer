6430d8aabe8ef40ed4cf4583e650597424dd4787
apollo78124 <98apollo@naver.com>
2020-11-13 23:45:01 -0800

    Gallery sliding animation project init

--- GalleryAppSlidingAnimation/app/src/main/java/com/example/galleryappeunhaklee/MainActivity.java
+++ GalleryAppSlidingAnimation/app/src/main/java/com/example/galleryappeunhaklee/MainActivity.java
@@ -20,25 +20,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageSwitcher;
 import android.widget.ImageView;
-import android.widget.TabHost;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView imageView1;
-    private Button buttonLeft,buttonRight, buttonSearch, buttonCamera, buttonTab;
+    private Button buttonLeft,buttonRight;
     private File storageDir;
     private String[] imageList;
     private int currentPicPosition;
     private File imgFile;
     private GestureDetectorCompat mDetector;
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-    static final int REQUEST_TAKE_PHOTO = 1;
-    String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,29 +71,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        buttonSearch =(Button) findViewById(R.id.searchButton);
-        buttonSearch.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                searchActivity();
-            }
-        });
-        buttonCamera =(Button) findViewById(R.id.cameraButton);
-        buttonCamera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                dispatchTakePictureIntent();
-            }
-        });
-        buttonTab =(Button) findViewById(R.id.tabButton);
-        buttonTab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                tabActivity();
-            }
-        });
-        mDetector = new GestureDetectorCompat(this, new MyGestureListener());
-
+            mDetector = new GestureDetectorCompat(this, new MyGestureListener());
     }
 
     private void imageLeft() {
@@ -142,14 +115,7 @@ public class MainActivity extends AppCompatActivity {
 
     class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
         private static final String DEBUG_TAG = "Gestures";
-        @Override
-        public boolean onDown(MotionEvent event) {
-            if (event.getX() < 350 && event.getY() < 1411 & event.getY() > 390)
-                imageLeft();
-            if (event.getX() > 774 && event.getY() < 1411 & event.getY() > 390)
-                imageRight();
-            return true;
-        }
+
         @Override
         public boolean onFling(MotionEvent event1, MotionEvent event2,
                                float velocityX, float velocityY) {
@@ -160,64 +126,6 @@ public class MainActivity extends AppCompatActivity {
                 imageRight();
             }
             return true;
-        }
-    }
-
-    public void searchActivity() {
-        Intent intent = new Intent(this, SearchActivity.class);
-        startActivity(intent);
-    }
-
-    public void tabActivity() {
-        Intent intent = new Intent(this, TabActivity.class);
-        startActivity(intent);
-    }
-
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        try {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        } catch (ActivityNotFoundException e) {
-            // display error state to the user
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            try {
-                createImageFile(imageBitmap);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            imageList = storageDir.list();
-            currentPicPosition = 0;
-        }
-    }
-
-    private void createImageFile(Bitmap bm) throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        currentPhotoPath = image.getAbsolutePath();
-        try {
-            FileOutputStream out = new FileOutputStream(image);
-            bm.compress(Bitmap.CompressFormat.JPEG, 100, out);
-            out.flush();
-            out.close();
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
